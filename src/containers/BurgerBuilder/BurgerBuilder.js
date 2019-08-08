271d31b3bff06c8fbc0fe3bc2e8d6de4be2cc1a6
RodrigoCornetaBWA <rodrigo.corneta@bwabrasil.com.br>
2019-08-08 14:36:44 -0300

    Criando components iniciais e corrigindo problema do component com nome de AUX no windows (152).

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import Auxi from '../../hoc/Auxi';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Auxi>
+                <div>Burger</div>
+                <div>Burger Controls</div>
+            </Auxi>
+        );
+    }
+}
+
+export default BurgerBuilder; 
