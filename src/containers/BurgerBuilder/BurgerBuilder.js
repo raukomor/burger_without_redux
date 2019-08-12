58a491b2e22e277aaab3fead27558ef352a2c642
RodrigoCornetaBWA <rodrigo.corneta@bwabrasil.com.br>
2019-08-12 14:05:10 -0300

    criação do componente da visualização do hamburger e componente do controle do hamburger (controles do hambuerger sem funcionalidades ainda) (159)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
 
 import Auxi from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+    //####old sintaxe
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 1,
+            meat: 0
+        }
+    }
+    
     render() {
         return (
             <Auxi>
-                <div>Burger</div>
-                <div>Burger Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
             </Auxi>
         );
     }
