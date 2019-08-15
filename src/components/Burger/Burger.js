377dc916fc46ecc143b55752d3ca304d793fba15
RodrigoCornetaBWA <rodrigo.corneta@bwabrasil.com.br>
2019-08-15 13:51:27 -0300

    Finalizando a responsividade e otimizando componentes (185)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,17 +7,16 @@ const burger = (props) => {
     let transformIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
-               return <BurgerIngredient key={igKey + i} type={igKey} />
+                return <BurgerIngredient key={igKey + i} type={igKey} />
             });
         })
         .reduce((arr, el) => {
             return arr.concat(el)
         }, []);
 
-        if(transformIngredients.length === 0){
-            transformIngredients = <p>Please start adding ingredients!</p>
-        }
-        console.log(transformIngredients);
+    if (transformIngredients.length === 0) {
+        transformIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Auxi from '../../../hoc/Auxi';
+import Auxi from '../../../hoc/Auxi/Auxi';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentWillUpdate(){
+        console.log('[OrderSummaty] WillUpdate');
+    }
+    
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                </li> );
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>);
 
         });
-    return (
-        <Auxi>
-            <h3>Your Order</h3>
-            <p>Adelicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType={"Danger"} clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType={"Success"} clicked={props.purchaseContinued}>CONTINUE</Button>
 
-        </Auxi>
-    )
+        return (
+            <Auxi>
+                <h3>Your Order</h3>
+                <p>Adelicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType={"Danger"} clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType={"Success"} clicked={this.props.purchaseContinued}>CONTINUE</Button>
+
+            </Auxi>
+        )
+    }
+
 }
 
-export default orderSummary; 
+export default OrderSummary; 
